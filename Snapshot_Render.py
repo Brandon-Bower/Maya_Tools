932e6b672d0a1ec30b7ba1eeb86ae02864689509
Brandon <BrandonKBower@gmail.com>
2022-03-16 15:16:59 -0700

    Can select root folder and print all contents

--- Snapshot_Render.py
+++ Snapshot_Render.py
@@ -1,9 +1,29 @@
 import maya.cmds as cmds
 import os
 
-def renderModels():
+WIDTH = 250 # constant for width of window
+
+def renderModels(*args):
+    path = cmds.button("pButton", q=True, l=True)
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        print(dirpath)
+        print(dirnames)
+        print(filenames)
     pass
 
+
+def findPath(*args):
+       '''
+       (str, bool) -> None
+       Finds path to first image in sequence, updates button label to that path
+       called by windowCall
+       '''
+       #multipleFilters = "Compatible files (*.jpg *.jpeg *.png);;jpeg (*.jpeg *.jpg);;png (*.png);;All Files (*.*)"
+       fPath = cmds.fileDialog2(cap='Select root folder', fm=2, ds=2)
+       if(fPath is not None):
+           cmds.button("pButton", e=True ,l=fPath[0])
+           cmds.button('Go', e=True, en=True)
+       return 
 
 def windowCall():
     '''
@@ -13,9 +33,11 @@ def windowCall():
     if cmds.window('Snapshot_Render', ex=True):
         cmds.deleteUI('Snapshot_Render') 
     cmds.window('Snapshot_Render')
-    cmds.columnLayout()
+    cmds.columnLayout("SnapRender_layout")
+    pathButton = cmds.button("pButton", l='Select root folder of objects to be rendered', w=WIDTH)
+    cmds.button(pathButton, e=True, c=findPath)
     cmds.button('Go', en=False, w=250, c=renderModels)
-    cmds.window('Snapshot_Render', e=True, h=20, w=250) 
+    cmds.window('Snapshot_Render', e=True, h=20, w=WIDTH) 
     cmds.showWindow('Snapshot_Render')
     return
 
