b157f3dd0fdec20f2f7bfc554ece4ec5e855db72
Brandon <BrandonKBower@gmail.com>
2022-03-17 00:05:30 -0700

    Renders and saves images of fbx files before deleting them

--- Snapshot_Render.py
+++ Snapshot_Render.py
@@ -4,11 +4,27 @@ import os
 WIDTH = 250 # constant for width of window
 
 def openRenderDelete(path, fbxFile):
-    fName = path + "/" + fbxFile
+    '''
+    (string, string) -> none
+    
+    imports fbxFile, renders an image of the file,
+    saves that image in the same folder as the file,
+    then deletes all imported elements
+    '''
+    # Complete path to fbx file
+    fName = f"{path}/{fbxFile}"
     imported = cmds.file(fName,  i=True, rnn=True)
+    # render image
+    cmds.render(cmds.listCameras()[0])
+    # example.fbx gets saved as example.png
+    fileSavePath = f"{path}/{os.path.splitext(fbxFile)[0]}"
+    # save render image
+    editor = 'renderView'
+    cmds.renderWindowEditor(editor, e=True, writeImage=fileSavePath)
+    # Select and delete all aspects of the imported meshes
     cmds.select(imported)
     cmds.Delete()
-    
+    return
     
 
 def renderModels(*args):
@@ -25,33 +41,31 @@ def renderModels(*args):
     path = cmds.button("pButton", q=True, l=True)
     # Walk through all sub-folders and files in root folder
     for (dirpath, dirnames, filenames) in os.walk(path):
-        print(dirpath)
-        print(dirnames)
-        print(filenames)
         for name in filenames:
+            # Only open fbx files
             if name.endswith('.fbx'):
                 openRenderDelete(dirpath, name)
-                
+    cmds.deleteUI('Snapshot_Render')             
     return
 
 
 def findPath(*args):
-       '''
-       (*args) -> None
-       
-       Finds path to first image in sequence, updates button label to that path
-       called by windowCall
-       
-       Note that *args are passed in, as Maya requires it, but are not used
-       '''
-       #multipleFilters = "Compatible files (*.jpg *.jpeg *.png);;jpeg (*.jpeg *.jpg);;png (*.png);;All Files (*.*)"
-       # Launch dialog to select root folder
-       fPath = cmds.fileDialog2(cap='Select root folder', fm=2, ds=2)
-       if(fPath is not None):
-           # Update path button and enable the 'Go' button
-           cmds.button("pButton", e=True ,l=fPath[0])
-           cmds.button('Go', e=True, en=True)
-       return 
+    '''
+    (*args) -> None
+    
+    Finds path to first image in sequence, updates button label to that path
+    called by windowCall
+    
+    Note that *args are passed in, as Maya requires it, but are not used
+    '''
+    #multipleFilters = "Compatible files (*.jpg *.jpeg *.png);;jpeg (*.jpeg *.jpg);;png (*.png);;All Files (*.*)"
+    # Launch dialog to select root folder
+    fPath = cmds.fileDialog2(cap='Select root folder', fm=2, ds=2)
+    if(fPath is not None):
+       # Update path button and enable the 'Go' button
+       cmds.button("pButton", e=True ,l=fPath[0])
+       cmds.button('Go', e=True, en=True)
+    return 
 
 def windowCall():
     '''
